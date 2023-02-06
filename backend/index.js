89722f07cbef6d7b68b5aeba4adc4e3b71dcb7f1
prabuhadi <prabuhadiyo@gmail.com>
2023-02-07 02:48:41 +0700

    Create backend user CRUD

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
+import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(UserRoute);
 
-app.listen(5050, () => console.log(`Server berhasil dijalankan..`));
+app.listen(5050, () => console.log(`Server berhasil dijalankan...`));
