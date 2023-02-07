99a3fe16c1153ab6c90c30c07fb77ff38fcda71b
prabuhadi <prabuhadiyo@gmail.com>
2023-02-07 17:12:09 +0700

    Create update profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserList from "./components/UserList";
 import AddUser from "./components/AddUser";
+import EditUser from "./components/EditUser";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<UserList />} />
         <Route path="/add" element={<AddUser />} />
+        <Route path="/edit/:id" element={<EditUser />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/EditUser.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+
+const EditUser = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("Male");
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    getUserById();
+  }, []);
+
+  const saveUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5050/users", {
+        name,
+        email,
+        gender,
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getUserById = async () => {
+    const response = await axios.get(`http://localhost:5050/users/${id}`);
+    setName(response.data.name);
+    setEmail(response.data.email);
+    setGender(response.data.gender);
+  };
+
+  return (
+    <div className="p-6">
+      <h1 className="has-text-centered has-text-weight-bold is-uppercase">
+        Edit User Profile
+      </h1>
+      <div className="columns mt-5 is-centered">
+        <div className="column is-half">
+          <form onSubmit={saveUser}>
+            <div className="field">
+              <label className="label">Name</label>
+              <div className="control">
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="input is-success"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control">
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="input is-success"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Gender</label>
+              <div className="control">
+                <div className="select is-success is-fullwidth">
+                  <select
+                    value={gender}
+                    onChange={(e) => setGender(e.target.value)}
+                  >
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="field">
+              <button type="submit" className="button px-6 mt-6 is-success">
+                Update
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditUser;
