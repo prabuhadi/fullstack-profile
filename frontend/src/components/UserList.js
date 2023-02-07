a64a79e8c937075e621b9a4f8c00a2bb6e7e23a7
prabuhadi <prabuhadiyo@gmail.com>
2023-02-07 17:49:10 +0700

    Create delete user function and fix table

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -14,6 +14,15 @@ export const UserList = () => {
     setUser(response.data);
   };
 
+  const deleteUser = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5050/users/${id}`);
+      getUsers();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="p-6">
       <h1 className="has-text-centered has-text-weight-bold is-uppercase">
@@ -48,7 +57,10 @@ export const UserList = () => {
                     >
                       Edit
                     </Link>
-                    <button className="button is-small is-danger">
+                    <button
+                      onClick={() => deleteUser(user.id)}
+                      className="button is-small is-danger"
+                    >
                       Delete
                     </button>
                   </td>
