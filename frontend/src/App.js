cc3fb0b3d5c1350bfb774a0123e335fa8a16bf99
prabuhadi <prabuhadiyo@gmail.com>
2023-02-07 13:58:12 +0700

    Install react.js, router-dom, axios, bulma and connecting to backend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import { UserList } from "./components/UserList";
+
+function App() {
+  return (
+    <div className="p-6">
+      <h1>Learning CRUD</h1>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/UserList.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export const UserList = () => {
+  const [users, setUser] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const response = await axios.get("http://localhost:5050/users");
+    console.log(response.data);
+  };
+
+  return (
+    <div className="columns mt-5 is-centered">
+      <div className="column is-half has-text-centered">
+        <table className="table is-striped is-fullwidth">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Gender</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+// export default UserList;
